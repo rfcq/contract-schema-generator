d92d22c51286209a793b7bea7fe1b48df30002b9
Rhuan Felipe <rhuanfelipe@outlook.com>
2022-02-15 09:00:02 -0300

    0.0.1

---
+++ src/converter.js
@@ -0,0 +1,12 @@
+const generateSchema = require('generate-schema')
+const fs = require('fs')
+
+let parameters = process.argv.slice(2)
+let inputFileName = parameters[0]
+let outputFileName = parameters[1]
+
+let fileInput = require(`../${inputFileName}`) 
+
+let schema = generateSchema.json(fileInput)
+
+const fileOutput = fs.writeFileSync(`./output/${outputFileName}`, JSON.stringify(schema))
